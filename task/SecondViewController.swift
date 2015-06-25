3a8b28a408f008b79bb9ba7933f1d35ad8dc5168
村上 雅典 <murakamimasanori@murakami-no-MacBook-Pro.local>
2015-06-26 01:02:55 +0900

    Initial Commit

---
+++ task/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  task
+//
+//  Created by 村上 雅典 on 2015/06/26.
+//  Copyright (c) 2015年 村上 雅典. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
